82993411cd60d6270d138f7c4aa6e50f4a18da55
mabiTLM <90094200+mabiTLM@users.noreply.github.com>
2025-07-10 09:17:48 +0900

    디데이 분 마다로 변경

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function find_day() {
 }
 
 find_day();
-setInterval(find_day, 1000);  // 초마다 디데이 기능 실행
+setInterval(find_day, 60000);  // 초마다 디데이 기능 실행
 
 function toggleAccount(accountId) {
   var accountInfo = document.getElementById(accountId);
